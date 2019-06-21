58a6857b3db02496eb525152c1592d2005a35c6d
Pandit_ab <Pandit_ab@Abhijeet-PN.India.XoriantCorp.com>
2019-06-21 19:01:16 +0530

    Added new project for java 8 demo

---
+++ Java8Demo1/src/MyClass.java
@@ -0,0 +1,32 @@
+public class MyClass {
+	private Integer intProperty = 0;
+	private String stringProperty = null;
+
+	public MyClass(int intProperty, String stringProperty) {
+		super();
+		this.intProperty = intProperty;
+		this.stringProperty = stringProperty;
+	}
+
+	public Integer getIntProperty() {
+		return intProperty;
+	}
+
+	public void setIntProperty(Integer intProperty) {
+		this.intProperty = intProperty;
+	}
+
+	public String getStringProperty() {
+		return stringProperty;
+	}
+
+	public void setStringProperty(String stringProperty) {
+		this.stringProperty = stringProperty;
+	}
+
+	@Override
+	public String toString() {
+		return "MyClass [intProperty=" + intProperty + ", stringProperty=" + stringProperty + "]";
+	}
+
+}
---
+++ Java8Demo1/src/TestLambda1.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class TestLambda1 {
+	public static void main(String[] args) {
+		System.out.println("Hellp");
+		MyOperations myOperation1 = (a, b) -> a * b;
+		System.out.println(myOperation1.operation(5, 10));
+		
+		List<MyClass> list = new ArrayList<>();
+		list.add(new MyClass(1, "One"));
+		list.add(new MyClass(2, "aaa"));
+		list.add(new MyClass(4, null));
+		list.add(new MyClass(3, "Three"));
+		list.add(new MyClass(4, "Four"));
+		list.add(null);
+		list.add(new MyClass(5, "Five"));
+		
+		System.out.println(list);
+		Collections.sort(list, Comparator.nullsFirst(Comparator.comparing(MyClass::getIntProperty, Comparator.reverseOrder()).thenComparing(Comparator.comparing(MyClass::getStringProperty, Comparator.nullsFirst(Comparator.naturalOrder())))));
+		/*
+		 * Comparator<String> comparator = new Comparator<String>() {
+		 * 
+		 * @Override public int compare(String o1, String o2) { // TODO Auto-generated
+		 * method stub return 0; } };
+		 */
+		System.out.println(list);
+	}
+
+	@FunctionalInterface
+	public interface MyOperations {
+		public int operation(int a, int b);
+	}
+}
